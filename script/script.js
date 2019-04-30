f7e57e07abecc4178edb872e9ae7b148a3f82ead
Just good boy <41293675+PresidentsBoyfriend@users.noreply.github.com>
2019-04-30 15:59:10 +0300

    pre-final commit

--- script/script.js
+++ script/script.js
@@ -3,16 +3,15 @@ let activePaint = false,
     activeMove = false, 
     activeTransform = false;
 
-//кнопки и окно
+
 let btnPaint = document.querySelector('.btn-paint'), 
     btnColor = document.querySelector('.btn-color'),
     btnMove = document.querySelector('.btn-move'),
     btnTransforn = document.querySelector('.btn-transform'),
     workPlace = document.querySelector('.workPlace'),
-    allPlace = document.body,
     btnPlace = document.querySelector('.btns');
 
-//Блоки
+
 let block0 = document.getElementById("block0").style.cssText = JSON.parse(localStorage.getItem("block0")) || " position: absolute; \
                                                                 top: 0;\
                                                                 left: 0;\
@@ -20,27 +19,27 @@ let block0 = document.getElementById("block0").style.cssText = JSON.parse(localS
                                                                 height: 200px;\
                                                                 width: 200px;\
                                                                 background: #5c5c5c;",
-    block1 = document.getElementById("block1").style.cssText =JSON.parse(localStorage.getItem("block1")) || "position: absolute;\
-     top: 0; left: 205px;\
-    margin: 5px;\
-    height: 200px;\
-    width: 200px;\
-    background: #5c5c5c;"
-    block2 = document.getElementById("block2").style.cssText =" position: absolute; \
+    block1 = document.getElementById("block1").style.cssText = JSON.parse(localStorage.getItem("block1")) || "position: absolute;\
+                                                                top: 0; left: 205px;\
+                                                                margin: 5px;\
+                                                                height: 200px;\
+                                                                width: 200px;\
+                                                                background: #5c5c5c;",
+    block2 = document.getElementById("block2").style.cssText = JSON.parse(localStorage.getItem("block2")) || "position: absolute; \
                                                                 top: 0;\
                                                                 left: 410px;\
                                                                 margin: 5px;\
                                                                 height: 200px;\
                                                                 width: 200px;\
                                                                 background: #5c5c5c;",
-    block3 = document.getElementById("block3").style.cssText =" position: absolute; \
+    block3 = document.getElementById("block3").style.cssText = JSON.parse(localStorage.getItem("block3")) ||" position: absolute; \
                                                                 top: 205px;\
                                                                 left: 0;\
                                                                 margin: 5px;\
                                                                 height: 200px;\
                                                                 width: 200px;\
                                                                 background: black;",
-    block4 = document.getElementById("block4").style.cssText =" position: absolute; \
+    block4 = document.getElementById("block4").style.cssText = JSON.parse(localStorage.getItem("block4")) ||" position: absolute; \
                                                                 top: 205px;\
                                                                 left: 205px;\
                                                                 margin: 5px;\
@@ -48,7 +47,7 @@ let block0 = document.getElementById("block0").style.cssText = JSON.parse(localS
                                                                 height: 200px;\
                                                                 width: 200px;\
                                                                 background: black;",
-    block5 = document.getElementById("block5").style.cssText =" position: absolute; \
+    block5 = document.getElementById("block5").style.cssText = JSON.parse(localStorage.getItem("block5")) ||" position: absolute; \
                                                                 top: 205px;\
                                                                 left: 410px;\
                                                                 margin: 5px;\
@@ -56,21 +55,21 @@ let block0 = document.getElementById("block0").style.cssText = JSON.parse(localS
                                                                 height: 200px;\
                                                                 width: 200px;\
                                                                 background: #5c5c5c;",
-    block6 = document.getElementById("block6").style.cssText =" position: absolute; \
+    block6 = document.getElementById("block6").style.cssText = JSON.parse(localStorage.getItem("block6")) ||" position: absolute; \
                                                                 top: 410px;\
                                                                 left: 0;\
                                                                 margin: 5px;\
                                                                 height: 200px;\
                                                                 width: 200px;\
                                                                 background: white;",
-    block7 = document.getElementById("block7").style.cssText =" position: absolute; \
+    block7 = document.getElementById("block7").style.cssText = JSON.parse(localStorage.getItem("block7")) ||" position: absolute; \
                                                                 top: 410px;\
                                                                 left: 205px;\
                                                                 margin: 5px;\
                                                                 height: 200px;\
                                                                 width: 200px;\
                                                                 background: #5c5c5c;",
-    block8 = document.getElementById("block8").style.cssText =" position: absolute; \
+    block8 = document.getElementById("block8").style.cssText = JSON.parse(localStorage.getItem("block8")) ||" position: absolute; \
                                                                 top: 410px;\
                                                                 left: 410px;\
                                                                 margin: 5px;\
@@ -79,14 +78,26 @@ let block0 = document.getElementById("block0").style.cssText = JSON.parse(localS
                                                                 width: 200px;\
                                                                 background: #5c5c5c;";
            
+
 let currentColor = document.querySelector('.currentDott'),
     prevColor =  document.querySelector('.prevDott'),
     redColor = document.querySelector('.redDott'),
-    blueColor = document.querySelector('.blueDott');
+    blueColor = document.querySelector('.blueDott'),
+    greenColor = document.querySelector('.greenDott'),
+    yellowColor = document.querySelector('.yellowDott');
+
+currentColor.style.cssText = JSON.parse(localStorage.getItem("currColor")) || "background: white;";
+prevColor.style.cssText = JSON.parse(localStorage.getItem("prevColor")) || "background: white;";
+
 
 let active;
+
+
 redColor.style.background = "red";
 blueColor.style.background = "blue";
+greenColor.style.background = "green";
+yellowColor.style.background = "yellow";
+
 
 btnPaint.addEventListener('click', function () {
     active = 'paint';
@@ -118,7 +129,19 @@ blueColor.addEventListener('click', function() {
     return currentColor.style.background = "blue";
 });
 
+greenColor.addEventListener('click', function() {
+    prevColor.style.background = currentColor.style.background;
+    return currentColor.style.background = "green";
+});
+
+yellowColor.addEventListener('click', function() {
+    prevColor.style.background = currentColor.style.background;
+    return currentColor.style.background = "yellow";
+});
+
+
 let firstItem, secondItem;
+
 workPlace.addEventListener('click', function (){
     if (active == "paint") {
         document.elementFromPoint(event.pageX, event.pageY).style.background=currentColor.style.background;
@@ -128,9 +151,9 @@ workPlace.addEventListener('click', function (){
         setTimeout (function() {
             active = undefined;
             btnColor.style.textDecoration  = "none";
+            return LocalStorage();
             }, 0);
         prevColor.style.background = currentColor.style.background;
-        console.log(document.elementFromPoint(event.pageX, event.pageY));
         currentColor.style.background = document.elementFromPoint(event.pageX, event.pageY).style.backgroundColor;
     }
     if (active == "move") {
@@ -143,7 +166,6 @@ workPlace.addEventListener('click', function (){
         else {
             secondItem = document.elementFromPoint(event.pageX, event.pageY);
             firstItem.style.boxShadow = "none";
-            //замена стилей позиции элементов
             timeTopFirst = firstItem.style.top;
             timeLeftFisrt = firstItem.style.left;
 
@@ -205,11 +227,50 @@ function Activated (active) {
 };
 
 function LocalStorage() {
-    block0 = document.getElementById("block0").style.cssText
+    currentColor = document.querySelector('.currentDott');
+    let curr = currentColor.style.cssText;
+    let currColor = JSON.stringify(curr);
+    localStorage.setItem("currColor", currColor);
+
+    prevColor =  document.querySelector('.prevDott');
+    let prev = prevColor.style.cssText;
+    let preColor  = JSON.stringify(prev);
+    localStorage.setItem("prevColor", preColor);
+
+    block0 = document.getElementById("block0").style.cssText;
     let itemBlock0 = JSON.stringify(block0);
     localStorage.setItem("block0", itemBlock0);
-    block1 = document.getElementById("block1").style.cssText
+
+    block1 = document.getElementById("block1").style.cssText;
     let itemBlock1 = JSON.stringify(block1);
     localStorage.setItem("block1", itemBlock1);
+
+    block2 = document.getElementById("block2").style.cssText;
+    let itemBlock2 = JSON.stringify(block2);
+    localStorage.setItem("block2", itemBlock2);
+
+    block3 = document.getElementById("block3").style.cssText;
+    let itemBlock3 = JSON.stringify(block3);
+    localStorage.setItem("block3", itemBlock3);
+
+    block4 = document.getElementById("block4").style.cssText;
+    let itemBlock4 = JSON.stringify(block4);
+    localStorage.setItem("block4", itemBlock4);
+
+    block5 = document.getElementById("block5").style.cssText;
+    let itemBlock5 = JSON.stringify(block5);
+    localStorage.setItem("block5", itemBlock5);
+
+    block6 = document.getElementById("block6").style.cssText;
+    let itemBlock6 = JSON.stringify(block6);
+    localStorage.setItem("block6", itemBlock6);
+
+    block7 = document.getElementById("block7").style.cssText;
+    let itemBlock7 = JSON.stringify(block7);
+    localStorage.setItem("block7", itemBlock7);
+
+    block8 = document.getElementById("block8").style.cssText;
+    let itemBlock8 = JSON.stringify(block8);
+    localStorage.setItem("block8", itemBlock8);
 };
 
